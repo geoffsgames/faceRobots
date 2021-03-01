45165f75b6b62eda49325910ba580e75e32074ce
geoffsgames <geoffne@whoever.com>
2021-03-01 17:28:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ app.use(express.static(__dirname));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+const players = {}
+var sokID = 0
+
+io.on('connection', function(socket){
+  socket.on('newPlayer', state => {
+    console.log('New player joined with state:', state)
+    players[socket.id] = sokID;
+    sokID += 1;
+  });
+  socket.on('move', function(msg){
+    io.emit('animate', {m: msg, s: players[socket.id]});
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
