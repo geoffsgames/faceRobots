751ac4f3e9f3d28c13920999c92e63c115a01ad4
geoffsgames <geoffne@whoever.com>
2021-03-05 23:05:42 +0000

    Update socEvents.js

--- socEvents.js
+++ socEvents.js
@@ -53,7 +53,7 @@ socket.on('newRival', function(msg){
 		msg.gr.top = (window.pageYOffset || document.documentElement.scrollTop) + 50;
 		canvas.add(msg.gr);
 		curRival = canvas._objects[canvas._objects.length - 1]; //you'd think curRival = msg.gr would work but there you go
-
+		canvas.remove(curRival);
 		//so new rival/player knows about me in return
 		socket.emit('newPlayer', {uID:uniqueID, gr:getStringArray(player.grid)});
 	}
