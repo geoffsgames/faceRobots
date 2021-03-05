6a3f18feb8975c559e93ecfb1d21c2540ca94a2f
geoffsgames <geoffne@whoever.com>
2021-03-05 17:28:03 +0000

    Update player.js

--- robots/player.js
+++ robots/player.js
@@ -600,8 +600,8 @@ Player.prototype.scrollInventory = function(absolute){
 		message.left = scrollLeft + (displayWidth/2);
 		message.top = scrollTop + 30;
 		if(curRival != undefined && curRival != null){
-			curRival.left = scrollLeft + (document.documentElement.clientWidth - 100);
-			curRival.top = scrollTop + 100;
+			curRival.left = scrollLeft + (document.documentElement.clientWidth - 10);
+			curRival.top = scrollTop + 10;
 		}
 		
 	}
