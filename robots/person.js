765ee22073d10c609870419931c4678d2222ce09
geoffsgames <geoffne@whoever.com>
2021-03-16 14:48:33 +0000

    Update person.js

--- robots/person.js
+++ robots/person.js
@@ -484,7 +484,8 @@ function allComplete(){
 	completeCounter += 1;
 	
 	if(completeCounter == numPlayers){
-
+		if(rivalTimeCounter != undefined)
+			rivalTimeCounter ++;
 /**
 		//hack to fix fact animation sometimes jumps me half way across the screen for some reason!
 		if(enemy.moved){
