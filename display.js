024b0eb8859ac87c1954147e1a9964956b8a3570
geoffsgames <geoffne@whoever.com>
2021-03-18 09:50:20 +0000

    Update display.js

--- display.js
+++ display.js
@@ -231,10 +231,8 @@ function keyListener(e){
 	    	code = e.keyCode;
 		if(enemy != null && !enemy.isEnemy)//multiplayer
 			savedKeyPress = {key:code, dc:new Date - lastTime < 500 && lastKey == code};
-		else{
-			savedKeyPress = {key:null, dc:0}
+		else
 			sendKeyPress(code,new Date - lastTime < 500 && lastKey == code) //if in multiplayer
-		}
 		changeState(code,new Date - lastTime < 500 && lastKey == code); //actually activate key code instruction - second parameter is true if doubleclicked
 		lastTime = new Date;
 		lastKey = code;
