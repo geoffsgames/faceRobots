47e2995ef64e4112ee34ce0145b892bad3f46aaa
geoffsgames <geoffne@whoever.com>
2021-03-19 03:34:33 +0000

    Update game.js

--- game.js
+++ game.js
@@ -69,7 +69,7 @@ var counter4KeyCmds = 0; //records which iteration we're on so key commands atta
 var keyMessage = null; //stores key commands received from rival
 var messageSent = false;
 
-var grrr = true;
+var grrr = false;
 
 start();
 addPlayer();
